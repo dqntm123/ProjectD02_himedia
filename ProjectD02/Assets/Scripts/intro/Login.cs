32a5ed5e61ea6168a7b0f26395ddebbcc5fe58ec
dqntm123 <36214326+dqntm123@users.noreply.github.com>
2018-06-18 18:34:55 +0900

    20180618
    12

--- ProjectD02/Assets/Scripts/intro/Login.cs
+++ ProjectD02/Assets/Scripts/intro/Login.cs
@@ -40,46 +40,46 @@ public class Login : MonoBehaviour
         Debug.Log("Editor 환경입니다.");
         StartCoroutine(StartLogin());
 #endif
-#if UNITY_ANDROID && !UNITY_EDITOR
-        Social.localUser.Authenticate((bool success) =>
-        {
-            if (success)
-            {             
-                Debug.Log("로그인 성공!");               
-                logText.text = "Login ID: " + Social.localUser.id;
-                StartCoroutine(StartLogin());
-            }
-            else
-            {
-                Debug.Log("로그인 실패!");              
-                logText.text = "Login failed!";
-            }
-        });
-#endif
+//#if UNITY_ANDROID && !UNITY_EDITOR
+//        Social.localUser.Authenticate((bool success) =>
+//        {
+//            if (success)
+//            {             
+//                Debug.Log("로그인 성공!");               
+//                logText.text = "Login ID: " + Social.localUser.id;
+//                StartCoroutine(StartLogin());
+//            }
+//            else
+//            {
+//                Debug.Log("로그인 실패!");              
+//                logText.text = "Login failed!";
+//            }
+//        });
+//#endif
     }
 
     IEnumerator StartLogin()
     {
         WWWForm form = new WWWForm();
-#if UNITY_ANDROID && !UNITY_EDITOR
-        form.AddField("GID", PlayerPrefs.GetString("GID"));
-#endif
+//#if UNITY_ANDROID && !UNITY_EDITOR
+//        form.AddField("GID", PlayerPrefs.GetString("GID"));
+//#endif
         form.AddField("uNum", PlayerPrefs.GetInt("uNum"));
         form.AddField("UUID", SystemInfo.deviceUniqueIdentifier);
         WWW www = new WWW(gameServerURL, form);    
         yield return www;  
         Debug.Log(www.text);
         SetMyGameData(www.text);       
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
         SceneManager.LoadScene(1);
     }
 
     public void SetMyGameData(string data)
     {
         var gameData = JSON.Parse(data);
-#if UNITY_ANDROID && !UNITY_EDITOR
-        PlayerPrefs.SetString("GID", Social.localUser.id);
-#endif
+//#if UNITY_ANDROID && !UNITY_EDITOR
+//        PlayerPrefs.SetString("GID", Social.localUser.id);
+//#endif
         PlayerPrefs.SetInt("uNum", int.Parse(gameData["uNum"]));
     }
 }
